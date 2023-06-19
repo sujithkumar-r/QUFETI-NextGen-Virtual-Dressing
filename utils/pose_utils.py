2f48df2a824bbbec5dab1874d79cfb77c0dfa02c
sujithkumar-r <130215533+sujithkumar-r@users.noreply.github.com>
2023-06-19 10:36:37 +0530

    Update pose_utils.py

--- utils/pose_utils.py
+++ utils/pose_utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter
-from skimage.draw import circle, line_aa, polygon
+from skimage.draw import disk, line_aa, polygon
 import json
 
 import matplotlib
@@ -88,7 +88,7 @@ def draw_pose_from_cords(pose_joints, img_size, radius=2, draw_joints=True):
     for i, joint in enumerate(pose_joints):
         if pose_joints[i][0] == MISSING_VALUE or pose_joints[i][1] == MISSING_VALUE:
             continue
-        yy, xx = circle(joint[0], joint[1], radius=radius, shape=img_size)
+        yy, xx = disk(joint[0], joint[1], radius=radius, shape=img_size)
         colors[yy, xx] = COLORS[i]
         mask[yy, xx] = True
 
